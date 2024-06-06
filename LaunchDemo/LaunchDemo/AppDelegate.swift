0b2a67ad98ccb88464b3bf272f9c9e94a95d5ff6
DaXueshan <401287246@qq.com>
2024-06-06 19:57:51 +0800

    启动框架在 demo 中应用

--- LaunchDemo/LaunchDemo/AppDelegate.swift
+++ LaunchDemo/LaunchDemo/AppDelegate.swift
@@ -11,13 +11,13 @@ import XSLauncher
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-//    TaskManager.registLaunchTasks()
+    TaskManager.registLaunchTasks()
     return true
   }
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
-//    XSLaunchManager.shared().executeLaunchItems(with: .didLaunch)
+    XSLaunchManager.shared().executeLaunchItems(with: .didLaunch)
 
 //    window?.rootViewController = HomeViewController()
     return true
